8c7f92ebef2492db6af4ae86b99871cc8f76becf
Gantogtokh <oganaa2472@gmail.com>
2026-01-10 13:28:23 -0600

    Initial commit

---
+++ test/aws-micservice.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsMicservice from '../lib/aws-micservice-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-micservice-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsMicservice.AwsMicserviceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
